d015c1322dd4a083e0793125df28968d4230f5c2
sihensom <166371356+sihensom@users.noreply.github.com>
2024-11-22 16:13:15 +0700

    Delete webBD directory

--- webBD/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Membuat koneksi ke database
-$conn = mysqli_connect(hostname: "localhost", username: "root", password: "", database: "ecommerce");
-
-// Cek koneksi
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-?>
